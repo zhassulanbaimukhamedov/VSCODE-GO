6f45b580a721f08543bcf03a01aa480349d2d821
zhassulanbaimukhamedov <zasulanb@gmail.com>
2021-08-18 19:09:52 +0600

    переменные

--- Основы языка Go/Переменные/main.go
+++ Основы языка Go/Переменные/main.go
@@ -1,9 +1,32 @@
 package main
 
-import "fmt"
-
 func main() {
-	var hello string
-	hello = "Hello world"
-	fmt.Println(hello)
+
+	// переменные применяются для хранения данных в программе
+
+	//var hello string // определение переменной
+	//hello = "Hello world" // присвоение значения
+	//fmt.Println(hello)
+
+	// можно сразу при объявлении переменной присвоить ей начальное значение
+	//var hello string = "Hello world"
+
+	// определение сразу несколько переменных, и присвоить им начальное значение
+	// var (
+	// 	name string = "Tom"
+	// 	age  int    = 27
+	// )
+	// fmt.Println(name)
+	// fmt.Println(age)
+
+	// значения можно многократно менять
+	// var hello string = "Hello world"
+	// fmt.Println(hello)
+	// hello = "Hello Go"
+	// fmt.Println(hello)
+
+	// краткое определение переменной
+	// hello := "Tom"
+	// fmt.Println(hello)
+
 }
