e5fd5839beffea2a4245f281e794fb02d2bf7a42
zhassulanbaimukhamedov <zasulanb@gmail.com>
2021-08-18 19:34:00 +0600

    типы данных

--- Основы языка Go/Типы данных/main.go
+++ Основы языка Go/Типы данных/main.go
@@ -6,4 +6,41 @@ func main() {
 
 	fmt.Println("Hello world")
 
+	// // целочисленные типы
+	// var a int8
+	// var b uint8
+	// var c byte = 3 // синоним типа uint8
+	// var d int16
+	// var f uint16
+	// var g int32
+	// var h rune // синоним типа int32
+	// var j uint32
+	// var k int64
+	// var l uint64
+	// var m int
+	// var n uint
+	// // числа с плавающей точкой
+	// var o float32
+	// var p float64
+	// // комплексные числа
+	// var q complex64
+	// var r complex128
+	// // тип bool
+	// var s bool
+	// // строки
+	// var t string
+	// // неявная типизация
+	// var name = "Tom"
+	// name := "Tom"
+	// //var name // ! Ошибка
+	// var name string         // надо либо указать тип
+	// var name = "Tom"        // либо начальное значение
+	// var name string = "Tom" // либо и то, и другое
+	// // неявная типизация нескольких переменных
+	// var (
+	// 	name = "Tom"
+	// 	age  = 27
+	// )
+	//var name, age = "Tom", 27
+
 }
